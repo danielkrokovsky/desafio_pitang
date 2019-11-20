062956b415eef40901057ff30f100c7545b6a89a
daniel <russour4@hotmail.com>
2019-11-20 12:25:35 -0300

    criação do projeto

---
+++ src/main/java/br/com/pitang/desafiopitang/DesafiopitangApplication.java
@@ -0,0 +1,13 @@
+package br.com.pitang.desafiopitang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiopitangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiopitangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pitang/desafiopitang/DesafiopitangApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pitang.desafiopitang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopitangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
