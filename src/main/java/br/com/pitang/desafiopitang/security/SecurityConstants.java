6771b39a39f47f2e073fb8b0801b9805fb818f30
daniel <danielkrokovsky@hotmail.com>
2019-11-26 18:19:10 -0300

    ajuste

--- src/main/java/br/com/pitang/desafiopitang/security/SecurityConstants.java
+++ src/main/java/br/com/pitang/desafiopitang/security/SecurityConstants.java
@@ -6,6 +6,6 @@ public class SecurityConstants {
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/signin";
-	public static final long EXPIRATION_TIME = 5000l;
+	public static final long EXPIRATION_TIME = 86400000l;
 
 }
